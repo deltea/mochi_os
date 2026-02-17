82d5e98f62e03a3b00983f45e267b1ff457f708e
deltea <thcheetah777@gmail.com>
2026-02-16 22:35:15 -0800

    fix weird issue with display

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,3 @@
-#define VS1053_RESET 27
-#define VS1053_CS 32
-#define VS1053_DCS 33
-#define CARD_CS 14
-#define VS1053_DREQ 15
-
-#define OLED_CS 15
-#define OLED_DC 33
-#define OLED_RST 27
-
-#define SCREEN_WIDTH 128
-#define SCREEN_HEIGHT 128
-#define FG 0xFFFF
-#define BG 0x0000
-
 #include <SPI.h>
 #include <SD.h>
 #include <Adafruit_SSD1351.h>
@@ -24,8 +9,23 @@
 #include "ui/screen.h"
 #include "ui/player_screen.h"
 
+#define VS1053_RESET 27
+#define VS1053_CS 32
+#define VS1053_DCS 33
+#define CARD_CS 14
+#define VS1053_DREQ 15
+
+#define OLED_CS 13
+#define OLED_DC 12
+#define OLED_RST 4
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 128
+#define FG 0xFFFF
+#define BG 0x0000
+
 Adafruit_VS1053_FilePlayer player = Adafruit_VS1053_FilePlayer(VS1053_RESET, VS1053_CS, VS1053_DCS, VS1053_DREQ, CARD_CS);
-// Adafruit_SSD1351 display = Adafruit_SSD1351(SCREEN_WIDTH, SCREEN_HEIGHT, &SPI, OLED_CS, OLED_DC, OLED_RST);
+Adafruit_SSD1351 display = Adafruit_SSD1351(SCREEN_WIDTH, SCREEN_HEIGHT, &SPI, OLED_CS, OLED_DC, OLED_RST);
 
 GFXcanvas16 currentFrame(SCREEN_WIDTH, SCREEN_HEIGHT);
 GFXcanvas16 lastFrame(SCREEN_WIDTH, SCREEN_HEIGHT);
@@ -40,25 +40,25 @@ uint32_t lastFrameTime = 0;
 
 void drawText(const char* text, int16_t x, int16_t y, bool centered = false, const GFXfont* font = nullptr, uint16_t color = FG) {
   if (font != nullptr) {
-    currentFrame.setFont(font);
+    display.setFont(font);
   } else {
     // monogram font is default
-    currentFrame.setFont(&monogram8pt7b);
+    display.setFont(&monogram8pt7b);
   }
 
-  currentFrame.setTextColor(color);
+  display.setTextColor(color);
 
   int16_t x1, y1;
   uint16_t w, h;
 
   if (centered) {
-    currentFrame.getTextBounds(text, 0, 0, &x1, &y1, &w, &h);
+    display.getTextBounds(text, 0, 0, &x1, &y1, &w, &h);
     // center horizontally
     x = x - (w / 2);
   }
 
-  currentFrame.setCursor(x, y);
-  currentFrame.print(text);
+  display.setCursor(x, y);
+  display.print(text);
 }
 
 void setup() {
@@ -67,15 +67,15 @@ void setup() {
   delay(500);
 
   // initialize display
-  // display.begin();
-  // display.fillScreen(BG);
-  // display.setSPISpeed(8000000);
-  // display.setTextColor(FG);
+  display.begin();
+  display.setSPISpeed(8000000);
+  display.fillScreen(BG);
+  display.setTextColor(FG);
 
-  // Serial.println("booting...");
-  // drawText("mochi_os", SCREEN_WIDTH / 2 + 8, SCREEN_HEIGHT / 2 - 4, true, &cute_pixel8pt7b, FG);
-  // drawText("booting up...", SCREEN_WIDTH / 2, SCREEN_HEIGHT - 10, true, nullptr, FG);
-  // display.drawRGBBitmap(SCREEN_WIDTH / 2 - 36, SCREEN_HEIGHT / 2 - 11, ICON_MUSIC, 16, 9);
+  Serial.println("booting...");
+  drawText("mochi_os", SCREEN_WIDTH / 2 + 8, SCREEN_HEIGHT / 2 - 4, true, &cute_pixel8pt7b, FG);
+  drawText("booting up...", SCREEN_WIDTH / 2, SCREEN_HEIGHT - 10, true, nullptr, FG);
+  display.drawRGBBitmap(SCREEN_WIDTH / 2 - 36, SCREEN_HEIGHT / 2 - 11, ICON_MUSIC, 16, 9);
 
   // initialize player
   if (!player.begin()) {
@@ -85,7 +85,7 @@ void setup() {
   Serial.println("player initialized!");
 
   // a tone to make sure the player is working
-  player.sineTest(0x44, 500);
+  player.sineTest(0x22, 200);
 
   // initialize sd card reader
   if (!SD.begin(CARD_CS)) {
@@ -96,9 +96,9 @@ void setup() {
 
   player.setVolume(60, 60);
 
-  // player.useInterrupt(VS1053_FILEPLAYER_PIN_INT);
+  // player.useInterrupt(VS1053_FILEPLAYER_TIMER0_INT);
   player.playFullFile("/track001.mp3");
-  player.playFullFile("/track002.mp3");
+  // player.playFullFile("/track002.mp3");
 
   currentScreen->init(state);
   lastFrameTime = millis();
@@ -136,16 +136,16 @@ void loop() {
   }
 
   // display updating
-  // uint16_t* curr = currentFrame.getBuffer();
-  // uint16_t* prev = lastFrame.getBuffer();
+  uint16_t* curr = currentFrame.getBuffer();
+  uint16_t* prev = lastFrame.getBuffer();
 
-  // for (int y = 0; y < SCREEN_HEIGHT; y++) {
-  //   for (int x = 0; x < SCREEN_WIDTH; x++) {
-  //     int index = y * SCREEN_WIDTH + x;
-  //     if (curr[index] != prev[index]) {
-  //       display.drawPixel(x, y, curr[index]);
-  //       prev[index] = curr[index];
-  //     }
-  //   }
-  // }
+  for (int y = 0; y < SCREEN_HEIGHT; y++) {
+    for (int x = 0; x < SCREEN_WIDTH; x++) {
+      int index = y * SCREEN_WIDTH + x;
+      if (curr[index] != prev[index]) {
+        display.drawPixel(x, y, curr[index]);
+        prev[index] = curr[index];
+      }
+    }
+  }
 }
